c49a817927852e509bade09df3031eeaa3311be8
khyeoklee0119 <hyogi0119@gmail.com>
2024-10-17 17:48:22 +0900

    add integration test
    Signed-off-by: khyeoklee0119 <hyogi0119@gmail.com>

--- model/loader/workaround_model_loader.go
+++
@@ -1,9 +0,0 @@
-package loader
-
-import "tensorflowGo/model"
-
-type WorkaroundModelLoader struct{}
-
-func (loader *WorkaroundModelLoader) Load(vo *ModelVO) model.Model{
-	return model.NewWorkAroundModel()
-}
